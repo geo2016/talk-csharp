144207339f6c446a5e37d58b1dc33a8a5fcdf784
Geo <348318920@qq.com>
2020-12-28 18:05:50 +0800

    更改了代码

---
+++ dotCoreConsole1/Car1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotCoreConsole1
+{
+    class Car1
+    {
+        private string _title;
+
+        public Car1()
+        {
+            _title = "No Name";
+        }
+        /// <summary>
+        /// 说一下自己的名字
+        /// </summary>
+        public void Say1()
+        {
+            Console.WriteLine($"I'm a {_title}.");
+        }
+
+        public void ChangeMyTitle(string newTitle)
+        {
+            _title = newTitle;
+        }
+    }
+}
--- dotCoreConsole1/Program.cs
+++ dotCoreConsole1/Program.cs
@@ -6,7 +6,35 @@ namespace dotCoreConsole1
     {
         static void Main(string[] args)
         {
+
+            int loop = 0;
+            while (loop<10)
+            {
+                Console.WriteLine($"loop value is {loop}");
+                loop++;
+            }
+            
+
+
+            
+
+            /*
+            int i1 = 10, i2 = 10;
+
+            int r1 = i1 / i2;
+
+            Console.WriteLine($"{i1} / {i2}的计算结果是 {r1}");
+            */
+
+            /*
+            Car1 car1 = new Car1();
+
+            car1.Say1();
+
+            car1.ChangeMyTitle("Red Name");
+            car1.Say1();
             Console.WriteLine("Hello 2021!");
+            */
         }
     }
 }
