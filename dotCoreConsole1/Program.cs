c6b2e6d7137010ad11c7ffb30ef74af8d9a4a433
Geo <348318920@qq.com>
2020-12-28 17:30:10 +0800

    新增项目 dotCoreConsole1

---
+++ dotCoreConsole1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotCoreConsole1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello 2021!");
+        }
+    }
+}
