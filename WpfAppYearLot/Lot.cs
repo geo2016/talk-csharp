b615fb13efe162bbec1ffbc739cbf6e0752e9640
Geo <348318920@qq.com>
2021-01-02 10:15:33 +0800

    创建了WPF抽奖工具版本

---
+++ WpfAppYearLot/Lot.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfAppYearLot
+{
+    class Lot
+    {
+        private List<string> giftForLot;
+        public List<string> GiftForLot { 
+            get {
+                return giftForLot;
+            }
+            set {
+                giftForLot = value;
+            }
+        }
+
+        
+
+        /// <summary>
+        /// 执行抽奖过程
+        /// </summary>
+        /// <param name="lotTimes">抽取多少次</param>
+        /// <returns>抽奖结果</returns>
+        public List<string> LotIt(int lotTimes)
+        {
+            List<string> result = new List<string>();
+
+            // 用来抽取的奖项
+            List<string> lotGifts = new List<string>(giftForLot);
+
+            // 确认抽奖次数不大于奖品数量
+            if (lotTimes > lotGifts.Count)
+            {
+                Console.WriteLine("抽奖次数不能大于奖品数量。");
+                return result;
+            }
+
+            Random rnd1 = new Random();
+
+            int loop = 0;
+            while (loop<lotTimes)
+            {
+                int giftUserLotIndex = rnd1.Next(lotGifts.Count);
+
+                result.Add($"第{loop+1}次抽中 {lotGifts[giftUserLotIndex]}");
+
+                lotGifts.RemoveAt(giftUserLotIndex);
+
+                loop++;
+            }
+
+
+            return result;
+        }
+    }
+}
---
+++ WpfAppYearLot/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppYearLot
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Lot _lot;
+        int _giftCount;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 初始化抽奖对象
+            _lot = new Lot();
+
+            // 从程序所在文件夹读取奖品列表
+            string pathApp = AppDomain.CurrentDomain.BaseDirectory;
+            string fileDataGift = pathApp + "gifts.txt";
+            if (File.Exists(fileDataGift))
+            {
+                string[] arr1 = File.ReadAllLines(fileDataGift);
+                _lot.GiftForLot = new List<string>(arr1);
+            }
+
+            _giftCount = _lot.GiftForLot.Count;
+        }
+
+        private void btnStartLot_Click(object sender, RoutedEventArgs e)
+        {
+            if(int.TryParse(tbTimesLot.Text,out int iTimesLot))
+            {
+                if(iTimesLot<1 || iTimesLot > _giftCount)
+                {
+                    MessageBox.Show("抽奖次数不能小于1或大于奖品数量。");
+                    tbTimesLot.Focus();
+                    return;
+                }
+
+                // 开始抽奖
+                listResult.Items.Clear();
+
+                foreach (var item in _lot.LotIt(iTimesLot))
+                {
+                    listResult.Items.Add(item);
+                }
+            }
+            else
+            {
+                MessageBox.Show("抽奖次数输入无效。");
+            }
+        }
+    }
+}
